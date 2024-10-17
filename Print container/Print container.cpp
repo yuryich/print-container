1d82efb25c9bfd5eed6543762804628f4466f2cc
Neto test <yury120405@gmail.com>
2024-10-17 21:08:42 +0400

    Initial cpmmit

---
+++ Print container/Print container.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+#include <string>
+
+template <typename Container>
+void print_container(const Container& container) {
+	auto it = container.begin(); 
+	while (it != container.end()) {
+		std::cout << *it;  
+		++it;  
+		if (it != container.end()) {
+			std::cout << ", ";  
+		}
+	}
+	std::cout << std::endl; 
+}
+
+int main() {
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	std::cout << "Set: ";
+	print_container(test_set);  
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	std::cout << "List: ";
+	print_container(test_list); 
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	std::cout << "Vector: ";
+	print_container(test_vector);  
+
+}
